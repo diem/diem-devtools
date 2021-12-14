473289795b8ba75f03284414c1551d14810acf09
bors-libra <53831230+bors-libra@users.noreply.github.com>
2021-12-14 23:21:42 +0000

    Deploying to gh-pages from  @ 0e5a9dcb914c103453ee314d79157349a313b310 🚀

---
+++ rustdoc/humantime_serde/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["deserialize","Deserializes a `Duration` or `SystemTime` via the humantime crate."],["serialize","Serializes a `Duration` or `SystemTime` via the humantime crate."]],"mod":[["re","Reexport module."]],"struct":[["Serde","A wrapper type which implements `Serialize` and `Deserialize` for types involving `SystemTime` and `Duration`."]]});
